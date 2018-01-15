639753118cbe9c58f686e8901f5ba8243c5c4ca4
Unknown <22047487+delta1512@users.noreply.github.com>
2018-01-15 22:32:48 +1100

    Added BOINC RPC password changing functionality

--- boincos-min/boinc.py
+++ boincos-min/boinc.py
@@ -10,6 +10,7 @@ import curses
 ### DEFINITIONS ###
 UP = curses.KEY_UP
 DN = curses.KEY_DOWN
+PASS_DIR = '/var/lib/boinc/gui_rpc_auth.cfg'
 
 ### START CODE ###
 def boinc_help():
@@ -27,12 +28,13 @@ def boinc_help():
         screen.addstr(1, 1, 'BOINC Tools:')
         screen.addstr(4, 3, '->\t BOINC Manager')
         screen.addstr(6, 3, '->\t Restart BOINC')
+        screen.addstr(8, 3, '->\t Change RPC password')
         screen.refresh()
         # Fetch and handle user selection
         selection = screen.getch(cursor[0], cursor[1])
-        if (selection == UP) and (4 < cursor[0] <= 6):
+        if (selection == UP) and (4 < cursor[0] <= 8):
             cursor[0] -= 2
-        elif (selection == DN) and (4 <= cursor[0] < 6):
+        elif (selection == DN) and (4 <= cursor[0] < 8):
             cursor[0] += 2
         elif (selection == ord(' ')):
             curses.endwin()
@@ -40,3 +42,25 @@ def boinc_help():
                 return 1 # Return case for starting boinctui
             elif (cursor[0] == 6):
                 return 2 # Return case for restarting BOINC
+            elif (cursor[0] == 8):
+                pass_scrn_sel = None
+                cursor = [4, 3]
+                screen.clear()
+                screen.border(0)
+                while pass_scrn_sel != ord('q'):
+                    with open(PASS_DIR, 'r') as pass_file:
+                        passwd = pass_file.read()
+                    screen.addstr(1, 1, 'Current password: ' + passwd)
+                    screen.addstr(4, 3, '->\t Change password')
+                    screen.refresh()
+                    pass_scrn_sel = screen.getch(cursor[0], cursor[1])
+                    if pass_scrn_sel == ord(' '):
+                        screen.clear()
+                        screen.border(0)
+                        screen.addstr(1, 1, 'New password:')
+                        screen.refresh()
+                        newpass = screen.getstr(1, 15)
+                        with open(PASS_DIR, 'w') as pass_file:
+                            pass_file.write(newpass)
+                        screen.clear()
+                        screen.border(0)
