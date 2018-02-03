491addb2d95387b0ff9d1bd576123a2dbc954517
Unknown <22047487+delta1512@users.noreply.github.com>
2018-02-03 22:50:00 +1100

    Fixed password shuffler

--- cross-distro/rpcshuffle.py
+++ cross-distro/rpcshuffle.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
-import subprocess as sp
-#from base64 import b64encode
-#from os import urandom
-#import string
+from base64 import b64encode
+from os import urandom
+import string
 
 newpass = False
 with open('/var/lib/boinc/gui_rpc_auth.cfg', 'r') as RPC_pass_file:
@@ -11,5 +10,4 @@ with open('/var/lib/boinc/gui_rpc_auth.cfg', 'r') as RPC_pass_file:
         newpass = True
 if newpass:
     with open('/var/lib/boinc/gui_rpc_auth.cfg', 'w') as RPC_pass_file:
-        #RPC_pass_file.write(b64encode(urandom(60)).decode('utf-8').translate(str.maketrans(string.punctuation, 'a'*32))[:8])
-        RPC_pass_file.write(sp.check_output('head -n 3 /dev/urandom | sha256sum', shell=True).decode()[:8])
+        RPC_pass_file.write(b64encode(urandom(60)).decode('utf-8').translate(dict((ord(char), None) for char in string.punctuation))[:8].encode('ascii', 'ignore'))
