7dc0f6ec2d4af35823e95137e07d3883df0c3587
delta1512 <22047487+delta1512@users.noreply.github.com>
2018-03-25 21:53:17 +1100

    Replaced all locales so the BOINC OS Helper language module works

--- boincos-min/chroot-install.sh
+++ boincos-min/chroot-install.sh
@@ -14,6 +14,7 @@ cp locale.gen /root/locale.gen.bak
 echo "en_US.UTF-8 UTF-8" > locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > locale.conf
+mv /root/locale.gen.bak /etc/locale.gen
 
 chmod +rw locale.gen locale.conf
 
